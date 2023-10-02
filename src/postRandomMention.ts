2d5233948fc5e1406a75762390eb05ea14037b36
Jooeun-K <gangjoo93@gmail.com>
2023-10-02 22:46:24 +0900

    refactor: postRandomToot의 config 가독성 증진

--- src/postRandomMention.ts
+++ src/postRandomMention.ts
@@ -15,11 +15,6 @@ const TOOT_LIST = [
   }
 ]
 
-const initTargetMention = (userUrl: string, userName: string) => {
-  const result = `<p><span class="h-card" translate="no"><a href="${userUrl}" class="u-url mention">@<span>${userName}</span></a></span>`
-  return result
-}
-
 type VisibilityType = Entity.Status['visibility']
 
 const BASE_URL = process.env.BASE_URL || ""
@@ -31,15 +26,11 @@ const selectRandomToot = () => {
   return TOOT_LIST[index].content
 }
 
-type TargetUser = {
-  url: string
-  name: string
-}
-
 const postRandomMention = async (reply_status_id: string, reply_username: string ,client?: MegalodonInterface, visibility?: VisibilityType) => {
   const targetClient = client || localClient 
   const toot = selectRandomToot()
-  await targetClient.postStatus(`@${reply_username} ${toot}`, {visibility: visibility || 'unlisted', in_reply_to_id: reply_status_id})
+  const postConfig = { visibility: visibility || 'unlisted', in_reply_to_id: reply_status_id }
+  await targetClient.postStatus(`@${reply_username} ${toot}`, postConfig)
 }
 
 export default postRandomMention
