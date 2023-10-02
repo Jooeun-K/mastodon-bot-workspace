f94df2911e25e2dfbd69d2a71f2fefb3394f246e
Jooeun-K <gangjoo93@gmail.com>
2023-10-02 21:31:27 +0900

    build: nodemon script 설정

--- src/getTimeline.ts
+++ src/getTimeline.ts
@@ -3,7 +3,7 @@ import { MegalodonInterface } from "megalodon";
 const getTimeline = async (client: MegalodonInterface) => {
   const res = await client.getHomeTimeline()
   res.data.forEach((status) => {
-    console.log("-----")
+    console.log("----- ------")
     console.log(status.account.username, "님의 게시글")
     console.log(status.content)
   })
